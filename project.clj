8bb411333d1d3d04dcf90c51d7eec4bda600e726
Max Gonzih <gonzih@gmail.com>
2013-09-19 11:07:17 +0300

    Update core.typed and hiccup

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [javax.mail/mail "1.4.7"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [hiccup "1.0.3"]
+                 [hiccup "1.0.4"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/core.typed "0.2.1"]]
+                 [org.clojure/core.typed "0.2.7"]]
   :main feeds2imap.core)
