bcbea5b4acfca61d28ddfb85cc1d1d62e52f4a53
Max Gonzih <gonzih@gmail.com>
2013-09-19 12:32:42 +0300

    Configure core.typed lein plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :core.typed {:check [feeds2imap.core
+                       feeds2imap.feeds
+                       feeds2imap.folder
+                       feeds2imap.imap
+                       feeds2imap.message
+                       feeds2imap.settings]}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [javax.mail/mail "1.4.7"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
