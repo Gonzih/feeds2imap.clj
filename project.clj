3419349fe1bc2d43445ec1540edebeab7b6d8cbb
Max Gonzih <gonzih@gmail.com>
2014-06-19 16:03:52 +0200

    Move core.typed to dev profile

--- project.clj
+++ project.clj
@@ -16,10 +16,10 @@
                  [hiccup "1.0.5"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j/log4j "1.2.17"]
-                 [digest "1.4.4"]
-                 [org.clojure/core.typed "0.2.48"]]
+                 [digest "1.4.4"]]
   :main feeds2imap.core
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [org.clojure/core.typed "0.2.48"]]
                    :plugins [[lein-midje "3.0.0"]]}
              :uberjar {:aot :all}}
   :min-lein-version "2.0.0")
