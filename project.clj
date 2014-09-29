a242734455c9e4d79f4527a388a3ae8546612ee2
Max Gonzih <gonzih@gmail.com>
2014-09-29 20:18:01 +0200

    Bump version (at least to create some activity)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feeds2imap "0.2.3"
+(defproject feeds2imap "0.2.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
