b105e591efe650a46f78a2edfb5406f19664a390
Max Gonzih <gonzih@gmail.com>
2014-10-26 09:05:32 +0100

    More no-check

--- src/feeds2imap/gpg.clj
+++ src/feeds2imap/gpg.clj
@@ -7,7 +7,7 @@
 (ann gpg-program String)
 (def gpg-program "gpg")
 
-(ann gpg-programm [String * -> ShellResult])
+(ann ^:no-check gpg-programm [String * -> ShellResult])
 (defn gpg
   "Shells out to (gpg-program) with the given arguments"
   [& args]
